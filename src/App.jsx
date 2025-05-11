e2fed280307b2ddd98de40da89884e17015305f4
your_username <malahat.m82@gmail.com>
2025-05-11 23:08:09 +0200

    move Header and Footer to Layout component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Product from './pages/Product'; 
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Cart from './pages/Cart';
 import Admin from './pages/Admin';
 import AddProduct from './pages/AddProduct';
 import AdminLogin from './pages/AdminLogin';
 
+import Layout from './components/Layout';
 
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-			<Route path="/products" element={<Product />} />
-			<Route path="/admin" element={<Admin />} />
-			<Route path="/add-product" element={<AddProduct />} />
-			<Route path="/admin-login" element={<AdminLogin />} />
-			
-
-			
-			
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+	  <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="products" element={<Product />} />
+        </Route>
+		<Route path="/admin" element={<Admin />} />
+        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+		</Routes>
     </Router>
   );
 }
 
 export default App;
-
